6c10a3bc9fa98809e0120c281ed0125856037806
Touseef <muhammad.touseef@tanits.org>
2023-01-19 20:33:29 +0500

    aadd sort and remove unnecessary code

--- controllers/post.contoller.js
+++ controllers/post.contoller.js
@@ -4,41 +4,20 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
 exports.createPost = async (req, res) => {
-    console.log("here in create post func.")
     let userId = req.user && req.user._id;
-    console.log("userId: " + userId)
-        // Post.create(req.body)
-        //     .then(post =>
-        //     {
-        //         res.status(200).send(post);
-        //     })
-        res.status(200).send({});
+    req.body.userId = userId;
+        Post.create(req.body)
+            .then(post =>
+            {
+                res.status(200).send(post);
+            })
 }
 
 exports.getPostsByUser = (req, res) => {
     const { user_id } = req;
     Post.find({ user_id })
+        .sort({createdAt: "desc"})
         .then(posts =>
             res.send(posts)
         )
-}
-
-generatejwt = (user) => {
-    const {email} = user;
-    return jwt.sign(
-        { user_id: user._id, email },
-        process.env.JWT_SECRET_KEY
-        // {
-        //     expiresIn: "2h",
-        // }
-    );
-}
-
-userView = (user, token) => {
-    return  {
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        token: token
-    };
 }
--- models/post.js
+++ models/post.js
@@ -1,9 +1,14 @@
-// var mongoose=require('mongoose');
-// var Post = new mongoose.Schema({
-//     title: String,
-//     description: String,
-//     userId: String,
-//     imageURL: String
-// });
-// module.exports = mongoose.model(
-//     'posts', Post, 'posts');
+var mongoose = require('mongoose');
+var Post = new mongoose.Schema({
+    content: String,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'userss',
+        required:true
+    },
+    imageURLs: Array
+    },
+    { timestamps: true }
+);
+module.exports = mongoose.model(
+    'posts', Post, 'posts');
