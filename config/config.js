2ead8f91eb2551be8ba1ed3cc32dd7fdeaec05b7
Touseef <muhammad.touseef@tanits.org>
2023-01-26 11:54:06 +0500

    add DB and collection llsiting scripts to config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,22 @@
 const dbConfig = () => {
     let mongoose = require('mongoose');
     let url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000';
+    //we can also specify DB name in URL like "mongodb://127.0.0.1:27017/authDev".
     mongoose.connect(url, {dbName: "authDev"});
     let db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function callback () {
         console.log("DB Connected");
+        //trying to get collection names
+        // mongoose.connection.db.listCollections().toArray(function (err, names) {
+        //   console.log(names); // [{ name: 'dbname.myCollection' }]
+        // });
+
+        //trying to get database
+        // var Admin = mongoose.mongo.Admin;
+        // new Admin(mongoose.connection.db).listDatabases(function(err, result) {
+        //   console.log(result.databases);
+        // });
     });
 };
 
